8d5d89c62290932682e22ae77c42f77e41e490cf
Ian Hsieh <ihsieh@pixar.com>
2023-11-01 12:39:05 -0700

    Fix relative path issue on Windows, reported on Discord, when using get_real_path.
    * We were only calling filesystem_path when the path we were getting was absolute.
    For now, get_real_path will now simply return filesystem_path.
    * This also fixes the issue where RIB files
    didn't get written. RixSceneGraph doesn't seem to like Windows backslashes.
    filesystem_path also turns all backslashes to forward slashes, for us.
    * Add a unit test to do some file path testing.

--- rfb_unittests/test_string_expr.py
+++ rfb_unittests/test_string_expr.py
@@ -1,6 +1,8 @@
 import unittest
 import bpy
-from ..rfb_utils import string_utils
+import os
+import pathlib
+from ..rfb_utils import string_utils, filepath_utils
 
 class StringExprTest(unittest.TestCase):
 
@@ -8,7 +10,8 @@ class StringExprTest(unittest.TestCase):
     def add_tests(self, suite):
         suite.addTest(StringExprTest('test_get_var'))
         suite.addTest(StringExprTest('test_set_var'))
-        suite.addTest(StringExprTest('test_expand_string'))
+        #suite.addTest(StringExprTest('test_expand_string'))
+        suite.addTest(StringExprTest('test_filepath'))
 
     # test getvar 
     def test_get_var(self):
@@ -27,3 +30,54 @@ class StringExprTest(unittest.TestCase):
         string_utils.set_var('unittest', 'StringExprTest')
         expanded_str = string_utils.expand_string(s, display='openexr', frame=1)
         self.assertEqual(expanded_str, compare)
+
+    # test filepath
+    def test_filepath(self):
+        # test whether '\' to '/'
+        s = r'C:\temp\foobar\blah.txt'
+        compare = 'C:/temp/foobar/blah.txt'
+        s = filepath_utils.get_real_path(s)
+        self.assertEqual(s, compare)
+
+        if not bpy.data.is_saved:
+            # cannot do the relative path test if scene is not saved
+            return 
+        
+        # test relative paths with //
+        bl_scene_file = bpy.data.filepath
+        bl_filepath = os.path.dirname(bl_scene_file)
+        compare = os.path.join(bl_filepath, 'volumes', 'foobar.txt')
+        try:
+            os.makedirs(os.path.join(bl_filepath, 'volumes'))
+        except:
+            pass
+        pathlib.Path(compare).touch()
+        s = r'//volumes\foobar.txt'
+        s = filepath_utils.get_real_path(s)
+        try:
+            self.assertEqual(s, compare)
+            os.remove(compare)
+            os.removedirs(os.path.join(bl_filepath, 'volumes'))
+        except AssertionError as e: 
+            os.remove(compare)        
+            os.removedirs(os.path.join(bl_filepath, 'volumes'))
+            raise
+
+        # test <blend_dir> substitution
+        compare = os.path.join('<blend_dir>', 'volumes', 'foobar.txt')
+        fp = os.path.join(bl_filepath, 'volumes', 'foobar.txt')
+        try:
+            os.makedirs(os.path.join(bl_filepath, 'volumes'))
+        except:
+            pass
+        pathlib.Path(fp).touch()
+        s = r'//volumes\foobar.txt'
+        s = filepath_utils.get_token_blender_file_path(s)     
+        try:
+            self.assertEqual(s, compare)
+            os.remove(fp)
+            os.removedirs(os.path.join(bl_filepath, 'volumes'))
+        except AssertionError as e: 
+            os.remove(fp)           
+            os.removedirs(os.path.join(bl_filepath, 'volumes'))
+            raise           
